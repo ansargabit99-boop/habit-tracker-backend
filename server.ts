dfa823d696a5693f38dd97f5c8464e445e904dd1
ansargabit99-boop <ansargabit99@gmail.com>
2026-09-17 12:11:44 +0500

    improving securirty

--- server.ts
+++ server.ts
@@ -3,11 +3,18 @@ import pool from "./data.ts"
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
 import bcrypt from 'bcryptjs'
+import rateLimit from 'express-rate-limit'
 dotenv.config()
 const app = express()
-app.use(express.json())
+app.use(express.json({limit:'10kb'}))
+app.set('trust proxy', 1)
+
+if (!process.env.JWT_SECRET) {
+  throw new Error('JWT_SECRET is not set')
+}
 
 const PORT = process.env.PORT || 5000
+const LoginLimitter = rateLimit({windowMs:15*60*100,max:10})
 
 function generateToken (id:number) {
     return jwt.sign({id},process.env.JWT_SECRET!,{
@@ -18,19 +25,23 @@ function generateToken (id:number) {
 app.post('/register',async(req,res)=>{
     try {
         const {name,gmail,number,password,maingoal,track,reminders,airespond} = req.body
+        if (password.length < 7) {
+            return res.status(400).json({message:'the password should contain atLeast 8 characters'})
+        }
         if(!name ||!gmail || !number ||!password||!maingoal||!track||!reminders||!airespond) {
             return res.status(400).json({message:'you didnt fill in expected forms'})
         }
-        const hashed = bcrypt.hash(password,10)
+        const hashed =await bcrypt.hash(password,10)
         const newUser = await pool.query('INSERT INTO users (name,gmail,number,password,maingoal,track,reminders,airespond) VALUES($1,$2,$3,$4,$5,$6,$7,$8) RETURNING *',[name,gmail,number,hashed,maingoal,track,reminders,airespond])
         const token = generateToken(newUser.rows[0].id)
-        res.json({data:newUser.rows[0],token})
+        const { password: _, ...safeUser } = newUser.rows[0]
+        res.json({ data: safeUser, token })
     } catch(err) {
         console.log(err)
         return res.status(500).json({message:"something went wrong"})
     }
 })
-app.post('/login',async(req,res)=>{
+app.post('/login',LoginLimitter,async(req,res)=>{
     try {
         const {gmailOrNum,password} = req.body
         let result:any
@@ -43,14 +54,14 @@ app.post('/login',async(req,res)=>{
             result = await pool.query('SELECT * FROM users WHERE number=$1',[gmailOrNum])
         }
         if(result.rows.length ===0) {
-            return res.status(404).json({message:'couldnt find user'})
+            return res.status(401).json({message:'invalid credentials'})
         }
         const isValid = await bcrypt.compare(password,result.rows[0].password)
         if(!isValid) {
-            return res.status(403).json({message:'invalid password'})
+            return res.status(401).json({message:'invalid credential'})
         }
         const token = generateToken(result.rows[0].id)
-        res.json({name:result.rows[0].name,token,id:result.json[0].id})
+        res.json({name:result.rows[0].name,token,id:result.rows[0].id})
 
     } catch(err) {
         console.log(err)
