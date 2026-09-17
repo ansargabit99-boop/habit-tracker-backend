251d18390ab329f1d922cfbb17161c66d2c36751
ansargabit99-boop <ansargabit99@gmail.com>
2026-09-17 10:15:21 +0500

    basic-api

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
- console.log('test')
+import express from "express"
+
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req, res) => {
+    res.send("Habit Tracker API is running!")
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
